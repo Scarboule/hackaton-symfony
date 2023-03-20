a104bd593bd43ac4c4b530033bb8f0925d85a36c
Oscar Dartigues <oscardartigues92@gmail.com>
2023-03-20 18:03:28 +0100

    add StationController

---
+++ src/Controller/StationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StationController extends AbstractController
+{
+    #[Route('/station', name: 'station_index')]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('station/index.html.twig', [
+            'controller_name' => 'StationController',
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/station/{id}', name: 'show_station')]
+    public function show($id, UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+        return $this->render('station/show.html.twig', [
+            'controller_name' => 'StationController',
+            'slopes' => $user->getSlopes(),
+            'lifts' => $user->getLifts(),
+
+        ]);
+    }
+}
