7c03d68a677c81162fe0ef37a5e69cf6950b48fc
Thea Blachon <thea.blachon73@gmail.com>
2023-03-20 18:03:30 +0100

    affichage station sur l'index

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+            'stations' => $userRepository->findAll(),
         ]);
     }
 }
