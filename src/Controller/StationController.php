357fbdcf04c9161b2ae90874edbae6e210707fe3
Oscar Dartigues <oscardartigues92@gmail.com>
2023-03-22 15:34:15 +0100

    :sparkles: add sorting slopes by difficulty feature

--- src/Controller/StationController.php
+++ src/Controller/StationController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-
-use App\Entity\User;
+use App\Repository\LiftRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,14 +21,48 @@ class StationController extends AbstractController
     }*/
 
     #[Route('/station/{id}', name: 'station_index')]
-    public function show($id, UserRepository $userRepository): Response
+    public function show($id, UserRepository $userRepository, LiftRepository $liftRepository): Response
     {
         $user = $userRepository->find($id);
+        $lifts = $user->getLifts();
+
+
+
         return $this->render('station/index.html.twig', [
             'controller_name' => 'StationController',
             'slopes' => $user->getSlopes(),
-            'lifts' => $user->getLifts(),
+            'lifts' => $lifts,
+            'id' => $id
 
+        ]);
+    }
+    #[Route('/station/{id}/{sort}', name: 'category_show_sorted')]
+    public function showSorted($id, $sort, UserRepository $userRepository): Response
+    {
+        $station = $userRepository->find($id);
+        $slopes = $station->getSlopes()->toArray();
+        $lifts = $station->getLifts();
+
+        if ($sort == 'asc') {
+            usort($slopes, function ($a, $b) {
+                $difficulties = ['green', 'blue', 'red', 'black'];
+                $aDiffIndex = array_search($a->getDifficulty(), $difficulties);
+                $bDiffIndex = array_search($b->getDifficulty(), $difficulties);
+                return $aDiffIndex - $bDiffIndex;
+            });
+        }elseif ($sort == 'desc'){
+            usort($slopes, function ($a, $b) {
+                $difficulties = ['green', 'blue', 'red', 'black'];
+                $aDiffIndex = array_search($a->getDifficulty(), $difficulties);
+                $bDiffIndex = array_search($b->getDifficulty(), $difficulties);
+                return  $bDiffIndex - $aDiffIndex;
+            });
+        }
+        return $this->render('station/index.html.twig', [
+            'controller_name' => 'StationController',
+            'slopes' => $slopes,
+            'lifts' => $lifts,
+            'id' => $id
         ]);
     }
 }
