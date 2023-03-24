e33b6c5fa37f29e938776e1bd77d8a40cc027c6f
Oscar Dartigues <oscardartigues92@gmail.com>
2023-03-24 17:12:00 +0100

    tailwind responsive struggling update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs' :{'max': '480px'},
+        'ss' :{'max': '720px'},
+        'tab':{'max': '1024px'},
+      },
     },
   },
   plugins: [],
